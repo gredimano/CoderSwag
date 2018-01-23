d16921141db11f011596d303f81767c2d02faa32
Generoso Redimano Jr <gredimano@objectbright.com>
2018-01-23 14:08:41 +0800

    Lesson 4.

--- app/src/main/java/com/redredimano/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/redredimano/coderswag/Controller/MainActivity.kt
@@ -2,20 +2,27 @@ package com.redredimano.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import com.redredimano.coderswag.Adapters.CategoryAdapter
+import com.redredimano.coderswag.Adapters.CategoryRecycleAdapter
 import com.redredimano.coderswag.R
 import com.redredimano.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+
+        categoryListView.setHasFixedSize(true)
     }
 }
